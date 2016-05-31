443cce2c58c7d4f526bc88f083695fddd16e571c
Jair Huaman <jairfrancesco@gmail.com>
2016-05-31 00:34:23 -0500

    update main on parser 2

--- Parser/main.cpp
+++ Parser/main.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
-//#include <string>
 #include <sstream>
 #include <stdio.h>
-//#include "Nodo_double.h"
-//#include "Nodo_funcion_binaria.h"
-//#include "Nodo_funcion_unaria.h"
-//#include "arbol_binario.h"
-//#include "Nodo.h"
-//#include "funciones.h"
 
 using namespace std;
 
@@ -98,31 +91,106 @@ double string_to_double(std::string strg){
 
 int main()
 {
-/*
-    Nodo * a= new Nodo_double(32);
-    Nodo * b= new Nodo_double(21);
-    Nodo * c= new Nodo_double(4);
-*/
+    try{
+        string expresion;
+        cin>>s;
+        LinkedBinaryTree *BT = new LinkedBinaryTree<string>();
+		//FileInputStream fstream = new FileInputStream("expressions.txt");
+	    //DataInputStream in = new DataInputStream(fstream);
+	    //BufferedReader br = new BufferedReader(new InputStreamReader(in));
+	    //String strLine;
+		//String conv[] = new String[50];
+		string conv[50];
+		//LinkedBinaryTree<String> BT = new LinkedBinaryTree<String>();
+		int iConv = 0;
+		if (expresion!=NULL)
+        {
+            cout<<endl;
+            conv[iConv] = "";
+            /*Infix to Prefix*/
+            stack<String> s_1 = new stack<String>();
+	        stack<String> s_2 = new stack<String>();
+	        int len = expresion.length();
+	        for_each(expresion.begin(), expresion.end(), [](char &n){
 
-    /*
-    double (*fun1)(double, double);
-    double (*fun2)(double);
-    fun1=&suma;
-    fun2=&cuadrado;
-    Nodo * d=new Nodo_funcion_binaria(fun1);
-    Nodo * e=new Nodo_funcion_unaria(fun2);
+                  });
+        }
+	    while ((strLine = br.readLine()) != null)   {
+	    	System.out.print(strLine + "\n");
+	    	conv[iConv] = "";
+	    	/*Infix to Prefix*/
 
-    */
+	        stack<String> s_1 = new stack<String>();
+	        stack<String> s_2 = new stack<String>();
+	        char ar[] = strLine.toCharArray();
+	        int len = ar.length;
+	        for(int i = len - 1; i >= 0; i--)
+	        {
+	        	if(ar[i] == ' ')
+	        		continue;
+	            switch(ar[i])
+	            {
+	                case ')':
+	                    s_1.push(String.valueOf(ar[i]));
+	                    break;
+	                case '(':
+	                {
+	                     while(!s_1.peek().equals(")"))
+	                     {
+	                         s_2.push(s_1.peek());
+	                         s_1.pop();
+	                     }
+	                     s_1.pop();
+	                     break;
+	                }
+	                case '+':  case '-':  case '*': case '/':
+	                {
+	                	if(s_1.size() == 0)
+	                		s_1.push(String.valueOf(ar[i]));
+	                	else{
+	                    while(p(s_1.peek().charAt(0)) > p(ar[i]))
+	                    {
+	                        s_2.push(s_1.peek());
+	                        s_1.pop();
+	                    }
+	                    s_1.push(String.valueOf(ar[i]));
+	                	}
+	                    break;
+	                }
+	                default:
+	                {
+	                    s_2.push(String.valueOf(ar[i]));
+	                    break;
+	                }
+	            }
 
-    /*
-    arbol_binario * tree= new arbol_binario(e);
-    tree->raiz->agregar_der(c);
-    tree->raiz->agregar_izq(d);
-    tree->raiz->izq->agregar_izq(a);
-    tree->raiz->izq->agregar_der(b);
-    */
+	        }
+	        while(!s_1.empty())
+	        {
+	            s_2.push(s_1.peek());
+	            s_1.pop();
+	        }
+	        while(!s_2.empty())
+	        {
+	            conv[iConv] += (s_2.peek());
+	            s_2.pop();
+	        }
 
+	    	System.out.println("Prefix notation = " + conv[iConv]);
+			for(int x = 0; x < conv[iConv].length(); x++){
 
+				BT.addNode(conv[iConv].substring(x, x+1));
+			}
+			System.out.print("Height = " + BT.getHeight() + "\n");
+
+			BT.BFS();
+			iConv++;
+			BT.destruct();
+	    }
+
+	}catch (exception& e){
+	    cout<<"Error: " + e.what() << endl();
+	}
     return 0;
 
 }
