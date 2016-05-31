3f1a54f8b080d9bc1a1be124585eb732bd9549df
texsmv <texs.mv@gmail.com>
2016-05-30 23:56:59 -0500

    Update main.cpp

--- Parser/main.cpp
+++ Parser/main.cpp
@@ -100,19 +100,40 @@ double string_to_double(std::string strg){
 
 int main()
 {
+        
+    /*  Primero que nada hay tres tipos de Nodo 
+    nodos a doubles, a funciones con un solo 
+    operando y a funciones con dos operandos
+    
+    y se declaran como los siguientes nodos
+    P.D : la creacion de los nodos las deberia hacer una clase automaticamente
+    */ 
+    
     Nodo * a= new Nodo_double(5);
     Nodo * b= new Nodo_double(2);
     Nodo * c= new Nodo_double(4);
     Nodo * d= new Nodo_double(6);
+    
+    //Las variables las declaras de la misma forma que un numero, dando como argumento el nombre de la variable
+    Nodo * variable= new Nodo_double("x");
 
-
+    /*Los nodos de funciones tienen como entrada funciones , 
+    de las cuales guardan un puntero, estas se encuentran en
+    include/funciones.h */
     Nodo * sum=new Nodo_funcion_binaria(suma);
     Nodo * cuadrad=new Nodo_funcion_unaria(cuadrado);
     Nodo * multi=new Nodo_funcion_binaria(multiplicacion);
 
-    Nodo * variable= new Nodo_double("x");
 
+    //aqui el arbol tiene como argumento el nodo que servira de raiz(siempre sera un nodo de funcion)
     arbol_binario * tree= new arbol_binario(sum);
+    
+    /*aqui se va creando el arbol
+    basicamente atravez de el arbol
+    se accede a los nodos y se agrega
+    el puntero a sus ramas izquierda 
+    y derecha
+    P.D : esto tambien deberia hacerlo una clase automaticamente */
     tree->raiz->agregar_der(cuadrad);
     tree->raiz->der->agregar_der(d);
     tree->raiz->agregar_izq(multi);
@@ -120,16 +141,25 @@ int main()
     tree->raiz->izq->agregar_der(variable);
     tree->obtener_variables();
 
+    /*Esta es la cosa que que te devuelve las coordenadas 
+    x,y de los puntos a graficar, tiene como argumento el puntero
+    a nuestro arbol, ademas posee metodos para definir los limites entre los 
+    que queremos hallar los puntos , y cada cuanto hallar un punto en los
+    limites dados(set_dx) */ 
     procesador_de_arbol * procesador= new procesador_de_arbol(tree);
     procesador->set_dx(0.7);
     procesador->set_limite_izq(150);
     procesador->set_limite_der(1500);
+    
+    //esto te devuelve un vector de tuplas con las coordenadas
     auto coor =procesador->get_coordenadas();
 
+    //esto es solo para mostrar los puntos que se hallan
     for (int i=0; i<coor.size();i++){
         std::cout<<  (std::get<0> (coor.at(i)) ) << " " << (std::get<1> (coor.at(i)) )<<endl ;
     }
-
+    
+    // Estas clases aun no son a prueba de fallos >_< 
 
     return 0;
 
