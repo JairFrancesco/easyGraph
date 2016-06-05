af36b3edd3ac06d7137b6efc5e21e61bb6949746
luisbch9 <luisbch9@gmail.com>
2016-06-05 12:51:33 -0500

    test x^2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,121 +1,89 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <vtkVersion.h>
+#include <vtkSmartPointer.h>
+#include <vtkCellArray.h>
+#include <vtkCellData.h>
+#include <vtkDoubleArray.h>
+#include <vtkPoints.h>
+#include <vtkPolyLine.h>
+#include <vtkPolyData.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkActor.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
-#include <vtkPolyData.h>
-#include <vtkSphereSource.h>
 #include <vtkAxesActor.h>
-#include <vtkTransform.h>
-#include <vtkSmartPointer.h>
-
-#include <vtkTransform.h>
-#include <vtkAxesActor.h>
-#include <vtkPolyDataMapper.h>
-#include <vtkLine.h>
 #include <iostream>
-
 #include <vector>
 using namespace std;
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
+      // Create a vtkPoints object and store the points in it
+      vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
 
-    vtkPoints * puntos =vtkPoints::New();
+      for(double i=0.0; i<5; i+=0.1){
+          double origin[3] = {0.0, i, i*i};
+          points->InsertNextPoint(origin);
+      }
 
-    for(double i=0; i<5;i++){
-       double p[3] = {0.0,i,i*i};
-      puntos->InsertNextPoint(p);
-    }
+      int numberids = 50;
+      vtkSmartPointer<vtkPolyLine> polyLine = vtkSmartPointer<vtkPolyLine>::New();
+      polyLine->GetPointIds()->SetNumberOfIds(numberids);
+      for(unsigned int i = 0; i < numberids; i++)
+      {
+        polyLine->GetPointIds()->SetId(i,i);
+      }
 
+      // Create a cell array to store the lines in and add the lines to it
+      vtkSmartPointer<vtkCellArray> cells = vtkSmartPointer<vtkCellArray>::New();
+      cells->InsertNextCell(polyLine);
 
-    vtkCellArray * lines =vtkCellArray::New();
-
-    for(unsigned int i = 0; i < 5; i++)
-     {
-       vtkLine * line = vtkLine::New();
-       line->GetPointIds()->SetId(i,i);
-       lines->InsertNextCell(line);
-    }
-
-    vtkPolyData * linesPolyData = vtkPolyData::New();
+      // Create a polydata to store everything in
+      vtkSmartPointer<vtkPolyData> polyData = vtkSmartPointer<vtkPolyData>::New();
 
       // Add the points to the dataset
-      linesPolyData->SetPoints(puntos);
+      polyData->SetPoints(points);
 
       // Add the lines to the dataset
-      linesPolyData->SetLines(lines);
+      polyData->SetLines(cells);
 
-     vtkPolyDataMapper * mapper = vtkPolyDataMapper::New();
-      mapper->SetInputData(linesPolyData);
-
-       vtkActor * actor  = vtkActor::New();
-       actor->SetMapper(mapper);
+      // Create two colors - one for each line
+      unsigned char red[3] = { 255, 0, 0 };
 
 
-     vtkSmartPointer<vtkSphereSource> sphereSource =
-     vtkSmartPointer<vtkSphereSource>::New();
-     sphereSource->SetCenter(0.0, 0.0, 0.0);
-     sphereSource->SetRadius(0.5);
+        // Create a vtkUnsignedCharArray container and store the colors in it
+        vtkSmartPointer<vtkUnsignedCharArray> colors = vtkSmartPointer<vtkUnsignedCharArray>::New();
+        colors->SetNumberOfComponents(3);
+        colors->InsertNextTupleValue(red);
 
-     //create a mapper
-     vtkSmartPointer<vtkPolyDataMapper> sphereMapper =
-       vtkSmartPointer<vtkPolyDataMapper>::New();
-     sphereMapper->SetInputConnection(sphereSource->GetOutputPort());
+      polyData->GetCellData()->SetScalars(colors);
 
-     // create an actor
-     vtkSmartPointer<vtkActor> sphereActor =
-       vtkSmartPointer<vtkActor>::New();
-     sphereActor->SetMapper(sphereMapper);
+      // Setup actor and mapper
+      vtkSmartPointer<vtkPolyDataMapper> mapper =  vtkSmartPointer<vtkPolyDataMapper>::New();
+      mapper->SetInputData(polyData);
 
-     // a renderer and render window
-     vtkSmartPointer<vtkRenderer> renderer =
-       vtkSmartPointer<vtkRenderer>::New();
-     this->ui->qvtkWidget->GetRenderWindow()->AddRenderer(renderer);
+
+      vtkSmartPointer<vtkActor> actor =
+        vtkSmartPointer<vtkActor>::New();
+      actor->SetMapper(mapper);
+
+      // Setup render window, renderer, and interactor
+      vtkSmartPointer<vtkRenderer> renderer =
+        vtkSmartPointer<vtkRenderer>::New();
 
 
 
-     // add the actors to the scene
-    // renderer->AddActor(sphereActor);
      renderer->AddActor(actor);
-
-     renderer->SetBackground(.1,.2,.3); // Background dark blue
-
-     vtkSmartPointer<vtkTransform> transform =
-       vtkSmartPointer<vtkTransform>::New();
-     transform->Translate(0.0, 0.0, 0.0);
-
-     vtkSmartPointer<vtkAxesActor> axes =
-       vtkSmartPointer<vtkAxesActor>::New();
-
-     // The axes are positioned with a user transform
-     axes->SetUserTransform(transform);
-
-     // properties of the axes labels can be set as follows
-     // this sets the x axis label to red
-     // axes->GetXAxisCaptionActor2D()->GetCaptionTextProperty()->SetColor(1,0,0);
-
-     // the actual text of the axis label can be changed:
-      axes->SetXAxisLabelText("Eje X");
-
-     renderer->AddActor(axes);
-
-     renderer->ResetCamera();
+     this->ui->qvtkWidget->GetRenderWindow()->AddRenderer(renderer);
      this->ui->qvtkWidget->GetRenderWindow()->Render();
 
-      // VTK/Qt wedded
-
-     // this->ui->qvtkWidget->GetRenderWindow()->AddRenderer(ren1);
-     // this->ui->qvtkWidget->GetRenderWindow()->Render();
-
-      // Set up action signals and slots
-      //connect(this->ui->actionExit, SIGNAL(triggered()), this, SLOT(slotExit()));
 }
 
 MainWindow::~MainWindow()
